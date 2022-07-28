6ec18f3455d0be0eb9de9c19a448a45703511d0d
pannncoba <109618566+pannncoba@users.noreply.github.com>
2022-07-29 03:16:44 +0700

    Update hisoka.js

--- hisoka.js
+++ hisoka.js
@@ -1,7 +1,7 @@
 /**
-   * Create By Dika Ardnt.
-   * Contact Me on wa.me/6288292024190
-   * Follow https://github.com/DikaArdnt
+   * Create By Pann.
+   * Contact Me on wa.me/18552042201
+   * Follow https://vt.tiktok.com/ZSRY9EH4v/
 */
 
 require('./config')
